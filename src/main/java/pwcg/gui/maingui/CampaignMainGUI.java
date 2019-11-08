313b71d6c6ea6a1da5bd0d34c554ba709a699c29
PWCGDeveloper <PatrickAWilson@outlook.com>
2019-11-08 13:40:41 -0500

    Change version

--- src/main/java/pwcg/gui/maingui/CampaignMainGUI.java
+++ src/main/java/pwcg/gui/maingui/CampaignMainGUI.java
@@ -58,7 +58,7 @@ import pwcg.gui.utils.ToolTipManager;
 public class CampaignMainGUI extends PwcgGuiContext implements ActionListener
 {
 	private static final long serialVersionUID = 1L;
-    private static final String VERSION = "   PWCG Version 7.1.0";
+    private static final String VERSION = "   PWCG Version 7.2.0";
 
 	private List<JButton> campaignButtonList = new ArrayList<JButton>();
 	
