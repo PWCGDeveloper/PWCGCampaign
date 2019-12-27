0e8f2c4c30fea42e1533a992c0a090eacc272ef8
Ian Caulfield <ian@murleen.org>
2019-12-27 20:52:40 +0000

    Airfield taxi path updates
    Add taxi paths for Bodenplatte, and also update the rules in the
    airfield test to try to avoid some taxi issues which have been reported.
    Taxi paths in existing maps updated to comply with these rules.
    Also move the fake airfield location to the parking location when using
    cold starts, as this avoids a weird issue with planes not moving at
    mission start.

--- src/test/java/pwcg/product/bos/airfield/BoSAirfieldTest.java
+++ src/test/java/pwcg/product/bos/airfield/BoSAirfieldTest.java
@@ -3,8 +3,11 @@ package pwcg.product.bos.airfield;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import pwcg.campaign.Campaign;
 import pwcg.campaign.context.IPWCGContextManager;
 import pwcg.campaign.context.PWCGContext;
 import pwcg.campaign.context.PWCGMap;
@@ -13,12 +16,18 @@ import pwcg.campaign.context.PWCGProduct;
 import pwcg.campaign.group.AirfieldManager;
 import pwcg.campaign.group.airfield.Airfield;
 import pwcg.campaign.group.airfield.Runway;
+import pwcg.core.config.ConfigItemKeys;
+import pwcg.core.config.ConfigManagerCampaign;
 import pwcg.core.exception.PWCGException;
 import pwcg.core.utils.MathUtils;
+import pwcg.testutils.CampaignCache;
+import pwcg.testutils.SquadronTestProfile;
 
 @RunWith(MockitoJUnitRunner.class)
 public class BoSAirfieldTest
 {
+    @Mock private ConfigManagerCampaign configManagerCampaign;
+
 	private static final String airfieldName = "Bahmutovo";
 	private static final String refChart =
 	        "    Chart\n" +
@@ -26,110 +35,122 @@ public class BoSAirfieldTest
 	        "      Point\n" +
 	        "      {\n" +
 	        "        Type = 0;\n" +
-	        "        X = 94.18;\n" +
-	        "        Y = -245.08;\n" +
+	        "        X = 96.34;\n" +
+	        "        Y = -245.55;\n" +
 	        "      }\n" +
 	        "      Point\n" +
 	        "      {\n" +
 	        "        Type = 1;\n" +
-	        "        X = 103.22;\n" +
-	        "        Y = -276.37;\n" +
+	        "        X = 105.38;\n" +
+	        "        Y = -276.83;\n" +
 	        "      }\n" +
 	        "      Point\n" +
 	        "      {\n" +
 	        "        Type = 1;\n" +
-	        "        X = 45.45;\n" +
-	        "        Y = -292.88;\n" +
+	        "        X = 47.62;\n" +
+	        "        Y = -293.34;\n" +
 	        "      }\n" +
 	        "      Point\n" +
 	        "      {\n" +
 	        "        Type = 1;\n" +
-	        "        X = -3.13;\n" +
-	        "        Y = -298.08;\n" +
+	        "        X = -0.97;\n" +
+	        "        Y = -298.54;\n" +
 	        "      }\n" +
 	        "      Point\n" +
 	        "      {\n" +
 	        "        Type = 1;\n" +
-	        "        X = -542.20;\n" +
-	        "        Y = -195.68;\n" +
+	        "        X = -270.50;\n" +
+	        "        Y = -247.34;\n" +
 	        "      }\n" +
 	        "      Point\n" +
 	        "      {\n" +
 	        "        Type = 1;\n" +
-	        "        X = -592.41;\n" +
-	        "        Y = -149.79;\n" +
+	        "        X = -540.04;\n" +
+	        "        Y = -196.15;\n" +
 	        "      }\n" +
 	        "      Point\n" +
 	        "      {\n" +
 	        "        Type = 1;\n" +
-	        "        X = -601.70;\n" +
-	        "        Y = -85.68;\n" +
+	        "        X = -590.24;\n" +
+	        "        Y = -150.25;\n" +
 	        "      }\n" +
 	        "      Point\n" +
 	        "      {\n" +
 	        "        Type = 1;\n" +
-	        "        X = -570.35;\n" +
-	        "        Y = -22.55;\n" +
+	        "        X = -599.54;\n" +
+	        "        Y = -86.14;\n" +
 	        "      }\n" +
 	        "      Point\n" +
 	        "      {\n" +
 	        "        Type = 1;\n" +
-	        "        X = -517.96;\n" +
-	        "        Y = -1.65;\n" +
+	        "        X = -568.19;\n" +
+	        "        Y = -23.01;\n" +
+	        "      }\n" +
+	        "      Point\n" +
+	        "      {\n" +
+	        "        Type = 1;\n" +
+	        "        X = -515.79;\n" +
+	        "        Y = -2.11;\n" +
 	        "      }\n" +
 	        "      Point\n" +
 	        "      {\n" +
 	        "        Type = 2;\n" +
-	        "        X = -466.68;\n" +
-	        "        Y = -1.58;\n" +
+	        "        X = -438.63;\n" +
+	        "        Y = -2.03;\n" +
 	        "      }\n" +
 	        "      Point\n" +
 	        "      {\n" +
 	        "        Type = 2;\n" +
-	        "        X = 466.68;\n" +
-	        "        Y = 1.58;\n" +
+	        "        X = 438.63;\n" +
+	        "        Y = 2.03;\n" +
 	        "      }\n" +
 	        "      Point\n" +
 	        "      {\n" +
 	        "        Type = 1;\n" +
-	        "        X = 519.46;\n" +
-	        "        Y = -2.74;\n" +
+	        "        X = 521.63;\n" +
+	        "        Y = -3.20;\n" +
 	        "      }\n" +
 	        "      Point\n" +
 	        "      {\n" +
 	        "        Type = 1;\n" +
-	        "        X = 572.11;\n" +
-	        "        Y = -27.05;\n" +
+	        "        X = 574.28;\n" +
+	        "        Y = -27.51;\n" +
 	        "      }\n" +
 	        "      Point\n" +
 	        "      {\n" +
 	        "        Type = 1;\n" +
-	        "        X = 600.00;\n" +
-	        "        Y = -77.89;\n" +
+	        "        X = 602.17;\n" +
+	        "        Y = -78.35;\n" +
 	        "      }\n" +
 	        "      Point\n" +
 	        "      {\n" +
 	        "        Type = 1;\n" +
-	        "        X = 595.85;\n" +
-	        "        Y = -139.31;\n" +
+	        "        X = 598.02;\n" +
+	        "        Y = -139.77;\n" +
 	        "      }\n" +
 	        "      Point\n" +
 	        "      {\n" +
 	        "        Type = 1;\n" +
-	        "        X = 547.18;\n" +
-	        "        Y = -191.71;\n" +
+	        "        X = 549.35;\n" +
+	        "        Y = -192.18;\n" +
 	        "      }\n" +
 	        "      Point\n" +
 	        "      {\n" +
 	        "        Type = 1;\n" +
-	        "        X = 102.91;\n" +
-	        "        Y = -277.03;\n" +
+	        "        X = 436.64;\n" +
+	        "        Y = -215.44;\n" +
+	        "      }\n" +
+	        "      Point\n" +
+	        "      {\n" +
+	        "        Type = 1;\n" +
+	        "        X = 105.08;\n" +
+	        "        Y = -277.50;\n" +
 	        "      }\n" +
 	        "      Point\n" +
 	        "      {\n" +
 	        "        Type = 0;\n" +
-	        "        X = 94.18;\n" +
-	        "        Y = -245.08;\n" +
+	        "        X = 96.34;\n" +
+	        "        Y = -245.55;\n" +
 	        "      }\n" +
 	        "    }\n";
 
@@ -142,6 +163,12 @@ public class BoSAirfieldTest
 	@Test
 	public void airfieldCheckMoscowTest() throws PWCGException
 	{
+        Campaign campaign = CampaignCache.makeCampaign(SquadronTestProfile.JG_51_PROFILE_MOSCOW);
+        campaign.setCampaignConfigManager(configManagerCampaign);
+        PWCGContext.getInstance().setCampaign(campaign);
+
+        Mockito.when(configManagerCampaign.getIntConfigParam(ConfigItemKeys.AllowAirStartsKey)).thenReturn(0);
+
         Airfield airfield = (Airfield) PWCGContext.getInstance().getAirfieldAllMaps(airfieldName);
 
 		PWCGContext.getInstance().getCurrentMap().getMapWeather().setWindDirection(180);
@@ -169,10 +196,10 @@ public class BoSAirfieldTest
 
 	                for (Runway runway : airfield.getAllRunways())
 	                {
-	                    // Runway start/end should be at least 50m from last taxi point
+	                    // Runway start/end should be at least 75m from last taxi point
 	                    // otherwise planes can take a long time to get into position
-	                    assert(MathUtils.calcDist(runway.getStartPos(), runway.getTaxiToStart().get(runway.getTaxiToStart().size() - 1)) >= 50);
-	                    assert(MathUtils.calcDist(runway.getEndPos(), runway.getTaxiFromEnd().get(0)) >= 50);
+	                    assert(MathUtils.calcDist(runway.getStartPos(), runway.getTaxiToStart().get(runway.getTaxiToStart().size() - 1)) >= 75);
+	                    assert(MathUtils.calcDist(runway.getEndPos(), runway.getTaxiFromEnd().get(0)) >= 75);
 
 	                    // Intermediate points on taxi paths should be closer to the next
 	                    // point than to the start of the runway, else planes will skip
@@ -181,13 +208,13 @@ public class BoSAirfieldTest
 	                    {
 	                        double distToNext = MathUtils.calcDist(runway.getTaxiToStart().get(i), runway.getTaxiToStart().get(i+1));
 	                        double distToRunway = MathUtils.calcDist(runway.getTaxiToStart().get(i), runway.getStartPos());
-	                        assert(distToNext < distToRunway);
+	                        assert(distToNext < (distToRunway - 5));
 	                    }
 	                    for (int i = 1; i < runway.getTaxiFromEnd().size(); i++)
 	                    {
 	                        double distToNext = MathUtils.calcDist(runway.getTaxiFromEnd().get(i), runway.getTaxiFromEnd().get(i-1));
 	                        double distToRunway = MathUtils.calcDist(runway.getTaxiFromEnd().get(i), runway.getEndPos());
-	                        assert(distToNext < distToRunway);
+	                        assert(distToNext < (distToRunway - 5));
 	                    }
 
 	                    // Angle from last taxi point to runway should not be too steep
@@ -195,8 +222,8 @@ public class BoSAirfieldTest
 	                    double angleToRunway = MathUtils.calcAngle(runway.getTaxiToStart().get(runway.getTaxiToStart().size() - 1), runway.getStartPos());
 	                    double angleFromRunway = MathUtils.calcAngle(runway.getEndPos(), runway.getTaxiFromEnd().get(0));
 
-	                    assert(MathUtils.calcNumberOfDegrees(angleToRunway, runwayAngle) < 45);
-	                    assert(MathUtils.calcNumberOfDegrees(runwayAngle, angleFromRunway) < 45);
+	                    assert(MathUtils.calcNumberOfDegrees(angleToRunway, runwayAngle) < 5);
+	                    assert(MathUtils.calcNumberOfDegrees(runwayAngle, angleFromRunway) < 5);
 	                }
 	            }
 	        }
