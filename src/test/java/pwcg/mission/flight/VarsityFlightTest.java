edfb717eefa324ffdc7c2e6b2cc7e668fc77f1fc
Patrick Wilson <PatrickAWilson@outlook.com>
2021-04-15 13:48:25 -0400

    Varsity

--- src/test/java/pwcg/mission/flight/VarsityFlightTest.java
+++ src/test/java/pwcg/mission/flight/VarsityFlightTest.java
@@ -9,54 +9,54 @@ import pwcg.campaign.Campaign;
 import pwcg.campaign.api.Side;
 import pwcg.campaign.context.PWCGContext;
 import pwcg.campaign.context.PWCGProduct;
-import pwcg.campaign.utils.TestDriver;
 import pwcg.core.exception.PWCGException;
 import pwcg.core.utils.DateUtils;
 import pwcg.core.utils.PWCGLogger;
 import pwcg.core.utils.PWCGLogger.LogLevel;
 import pwcg.mission.Mission;
 import pwcg.mission.MissionGenerator;
+import pwcg.mission.target.TargetType;
 import pwcg.mission.utils.MissionFlightValidator;
 import pwcg.mission.utils.MissionInformationUtils;
 import pwcg.testutils.CampaignCache;
 import pwcg.testutils.SquadronTestProfile;
 import pwcg.testutils.TestMissionBuilderUtility;
 
-public class PlunderInvasionFlightTest
+public class VarsityFlightTest
 {
     @Before
-    public void fighterFlightTests() throws PWCGException
+    public void setup() throws PWCGException
     {
         PWCGContext.setProduct(PWCGProduct.BOS);
         PWCGLogger.setActiveLogLevel(LogLevel.DEBUG);
     }
 
     @Test
-    public void plunderHasGroundAttackTest() throws PWCGException
+    public void hasSkirmishAndParaDropTest() throws PWCGException
+    {
+        verifyParaDropOnDate(DateUtils.getDateYYYYMMDD("19450324"));
+    }
+
+    private void verifyParaDropOnDate(Date date) throws PWCGException
     {
         Campaign campaign = CampaignCache.makeCampaign(SquadronTestProfile.RAF_184_PROFILE);
-        campaign.setDate(DateUtils.getDateYYYYMMDD("19450323"));
+        campaign.setDate(date);
         MissionGenerator missionGenerator = new MissionGenerator(campaign);
         Mission mission = missionGenerator.makeMission(TestMissionBuilderUtility.buildTestParticipatingHumans(campaign));
 
         assert (mission.getSkirmish() != null);
-        assert (mission.getSkirmish().getSkirmishName().contains("Plunder"));
+        assert (mission.getSkirmish().getSkirmishName().contains("Varsity"));
 
-        boolean axisGroundAttackFlightFound = MissionInformationUtils.verifyFlightTypeInMission(mission, FlightTypes.GROUND_ATTACK, Side.AXIS);
-        assert (axisGroundAttackFlightFound);
-
-        boolean alliedGroundAttackFlightFound = MissionInformationUtils.verifyFlightTypeInMission(mission, FlightTypes.GROUND_ATTACK, Side.ALLIED);
-        assert (alliedGroundAttackFlightFound);
-
+        assert(MissionInformationUtils.verifyFlightTypeInMission(mission, FlightTypes.PARATROOP_DROP, Side.ALLIED));
+        assert(MissionInformationUtils.verifyFlightTargets(mission, FlightTypes.GROUND_ATTACK, TargetType.TARGET_INFANTRY, Side.ALLIED));
+        assert(MissionInformationUtils.verifyFlightTargets(mission, FlightTypes.BOMB, TargetType.TARGET_INFANTRY, Side.ALLIED));
         MissionFlightValidator.validateMission(mission);
-        
-        TestDriver.saveTestMission(campaign, mission);
     }
 
     @Test
-    public void doesNotHaveSkirmishBeforeTest() throws PWCGException
+    public void doesNotHaveSkirmishAfterTest() throws PWCGException
     {
-        noSkirmish(DateUtils.getDateYYYYMMDD("19450322"));
+        noSkirmish(DateUtils.getDateYYYYMMDD("19450325"));
     }
 
     private void noSkirmish(Date date) throws PWCGException
